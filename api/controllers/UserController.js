f34dd1637aa065bc7c786b299f47125c93577a6e
Wouter <tuvokki@gmail.com>
2015-03-17 21:04:18 +0100

    Login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,28 +7,25 @@
 
 module.exports = {
 	  login: function (req, res) {
-    var bcrypt = require('bcrypt');
+    // var bcrypt = require('bcrypt');
 
-    User.findOneByEmail(req.body.email).done(function (err, user) {
+    User.findOneByEmail(req.body.email).exec(function (err, user) {
       if (err) res.json({ error: 'DB error' }, 500);
 
       if (user) {
         // compare the hashed password in the db with the one in the form
         console.log("Form pass hash: ", User.hash_string(req.body.password));
         console.log("DB pass hash:   ", user.password);
-        // bcrypt.compare(req.body.password, user.password, function (err, match) {
-        //   if (err) res.json({ error: 'Server error' }, 500);
 
-        //   if (match) {
-        //     // password match
-        //     req.session.user = user.id;
-        //     res.json(user);
-        //   } else {
-        //     // invalid password
-        //     if (req.session.user) req.session.user = null;
-        //     res.json({ error: 'Invalid password' }, 400);
-        //   }
-        // });
+        if (User.hash_string(req.body.password) === user.password) {
+          // password match
+          req.session.user = user.id;
+          res.json(user);
+        } else {
+          // invalid password
+          if (req.session.user) req.session.user = null;
+          res.json({ error: 'Invalid password' }, 400);
+        }
       } else {
         res.json({ error: 'User not found' }, 404);
       }
