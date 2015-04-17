c7fcd873778557b44f53898290120e45af7700d2
Wouter <tuvokki@gmail.com>
2015-04-17 22:43:32 +0200

    Store the criteria in the request for later use

--- api/policies/collectParams.js
+++ api/policies/collectParams.js
@@ -12,7 +12,12 @@ module.exports = function (req, res, next) {
     // collect all params
     criteria = _.merge({}, req.params.all(), req.body);
     // store the criteria somewhere for later use
-    // or perhaps pass them on to the next call
+    // The values in req.options.values are used as defaults for the request
+    if(!req.options.values)
+      req.options.values = {};
+    
+    req.options.values.criteria = criteria;
+
     console.log(req.options.model);      // Model name - if you are using blueprints
     console.log(req.options.controller); // Controller name
     console.log(req.options.action);     // Action name
