9ba4be0da67b814f11277a38b8a9ac34535369ec
Wouter <tuvokki@gmail.com>
2015-04-22 23:04:47 +0200

    Check auth header on data requests fixes #4

---
+++ api/services/passport.js
@@ -0,0 +1,22 @@
+var passport = require('passport'),
+  BearerStrategy = require('passport-http-bearer').Strategy;
+
+/**
+ * BearerStrategy
+ *
+ * This strategy is used to authenticate either users or clients based on an access token
+ * (aka a bearer token).  If a user, they must have previously authorized a client
+ * application, which is issued an access token to make requests on behalf of
+ * the authorizing user.
+ */
+passport.use('bearer', new BearerStrategy(
+  function(accessToken, done) {
+    console.log("accessToken", accessToken);
+    User.findOne( { token: accessToken }, function(err, user) {
+      if (err) return done(err);
+      console.log("user", user);
+      var info = { scope: '*' };
+      done(null, user, info);
+    });
+  }
+));
--- config/policies.js
+++ config/policies.js
@@ -30,7 +30,7 @@ module.exports.policies = {
 
   // Policy for all actions of a specific controller
   'DataController': {
-      '*': 'collectParams'
+      '*': ['collectParams', 'authenticated']
   },
 
 
