a43fd332372819d8d618d1d71f43070655748bce
Wouter Roosendaal <tuvokki@gmail.com>
2015-04-24 16:08:48 +0200

    Revert "Fix"
    This reverts commit 744681cca0e445029b5f5e3015f99aa7f35293e2.
    
    Conflicts:
    	config/connections.js

--- config/connections.js
+++ config/connections.js
@@ -19,9 +19,12 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.connections.html
  */
 
+<<<<<<< HEAD
 require('dotenv').load();
 var dbUrl = 'mongodb://%s:%s@%s:%s/%s', process.env.DB_USER, process.env.DB_PASS, process.env.DB_HOST, process.env.DB_PORT, process.env.DB_NAME;
 
+=======
+>>>>>>> parent of 744681c... Fix
 module.exports.connections = {
 
   /***************************************************************************
@@ -59,6 +62,7 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
+  var dbUrl = 'mongodb://%s:%s@%s:%s/%s', process.env.DB_USER, process.env.DB_PASS, process.env.DB_HOST, process.env.DB_PORT, process.env.DB_NAME;
   hunaMongodbServer: {
     adapter: 'sails-mongo',
     host: 'localhost',
