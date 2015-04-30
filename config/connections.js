8a0e15d31f8f96b4428b78ce7206146156be552e
Wouter Roosendaal <tuvokki@gmail.com>
2015-04-30 13:06:00 +0200

    Read settings from .env

--- config/connections.js
+++ config/connections.js
@@ -18,11 +18,11 @@ module.exports.connections = {
   *                                                                          *
   ***************************************************************************/
   hunaMongodbServer: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    database: 'huna-dev'
+    adapter:  'sails-mongo',
+    host:     process.env.DB_HOST,
+    port:     process.env.DB_PORT,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   },
 };
