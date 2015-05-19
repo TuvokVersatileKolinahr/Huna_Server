0db80f132a8367861428f64171526ac4bcaa2297
Wouter <tuvokki@gmail.com>
2015-05-20 00:53:41 +0200

    Add create method to host controller

--- api/controllers/HostController.js
+++ api/controllers/HostController.js
@@ -6,5 +6,17 @@
  */
 
 module.exports = {
-
+  create: function(req, res) {
+    var accessToken = req.get('Authorization').split(" ")[1];
+    User.findOne( { token: accessToken }, function(err, user) {
+      if (err) {
+        console.log("Find user on host create failed.", err);
+        res.status(500).json({ error: 'DB error' });
+      }
+      else {
+        console.log("create host for user", user);
+        res.status(200).json({ host: 'true' });
+      }
+    });
+  }
 };
