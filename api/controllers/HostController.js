4b2cde7e574595c4ba965bf20898bdfd7af87054
Wouter <tuvokki@gmail.com>
2015-06-09 23:07:48 +0200

    Create host by user with id

--- api/controllers/HostController.js
+++ api/controllers/HostController.js
@@ -9,10 +9,11 @@ module.exports = {
   create: function(req, res) {
     console.log("User: ", req.user);
     var newhost = req.body;
-    newhost.owner = req.user;
+    newhost.owner = req.user.id;
+    console.log("newhost", newhost);
     Host.create(newhost).exec(function(err, host){
       if (err) {
-        console.log("Jammer. Create host failed", err);
+        console.log("Create host failed", err);
         res.status(500).json({ error: "Error creating host" });
       }
       else res.status(200).json({ host: host });
