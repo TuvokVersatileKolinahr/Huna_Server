f64b2c806b01a159131dd647c6d549f606ad14af
Wouter <tuvokki@gmail.com>
2015-06-09 23:06:38 +0200

    Add owner correctly to host model

--- api/models/Host.js
+++ api/models/Host.js
@@ -18,10 +18,10 @@ module.exports = {
     name : {
       type: 'string',
       required: true,
-      unique: true,
-      owner:{
-          model:'user'
-      }
+      unique: true
+    },
+    owner:{
+      model:'user'
     }
   }
 };
